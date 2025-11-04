451b97f13e539f604a4f181d0a013e5b4664ef7e
Firnando93 <nando93firnando@gmail.com>
2025-11-04 10:06:06 +0700

    Update learn.js

--- Learn-JS/learn.js
+++ Learn-JS/learn.js
@@ -6,27 +6,61 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Foccacia", "Brushcetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 };
 
-const arr = [2, 3, 4];
-const [x, y, z] = arr;
-console.log(x, y, z);
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 
-const [first, second] = restaurant.categories;
-console.log(first, second);
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
 
-// Receive 2 return values
-const [starterCourse, mainCourse] = restaurant.order(2, 0);
-console.log(starterCourse, mainCourse);
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 11, b: 22, c: 33 };
+({ a, b } = obj);
+console.log(a, b);
 
-// Nested destructuring
-const nested = [2, 3, 4, [5, 6]];
-const [i, , j, [k, l]] = nested;
-console.log(i, j, k, l);
+//Nested objets
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
 
-// Default Value
-const [a = 1, b = 2, c = 3] = [8, 9];
-console.log(a, b, c);
+// const arr = [2, 3, 4];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+// // Receive 2 return values
+// const [starterCourse, mainCourse] = restaurant.order(2, 0);
+// console.log(starterCourse, mainCourse);
+
+// // Nested destructuring
+// const nested = [2, 3, 4, [5, 6]];
+// const [i, , j, [k, l]] = nested;
+// console.log(i, j, k, l);
+
+// // Default Value
+// const [a = 1, b = 2, c = 3] = [8, 9];
+// console.log(a, b, c);
